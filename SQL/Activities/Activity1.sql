a969b812d61707d5218ece6d97e6fcb845ad5bd0
Thipparti32 <116862262+Thipparti32@users.noreply.github.com>
2022-11-14 17:24:16 +0530

    Add files via upload
    Sql Activities

---
+++ SQL/Activities/Activity1.sql
@@ -0,0 +1,10 @@
+REM   Script: Session 01
+REM   FST MODULE
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
